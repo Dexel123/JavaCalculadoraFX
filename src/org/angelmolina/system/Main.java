a49ed5a2b9ac0d9cad9b061b0fb9713c50309b2f
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-06-12 11:25:51 -0600

    Primer commit , creacion del proyecto #000

---
+++ src/org/angelmolina/system/Main.java
@@ -0,0 +1,33 @@
+
+package org.angelmolina.system;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage  escenarioPrincipal) throws Exception {
+        VBox raiz = new VBox();
+        
+        Scene escena = new Scene(raiz);
+        
+        escenarioPrincipal.setTitle("Calculadora de Angel");
+       escenarioPrincipal.setScene(escena); 
+        escenarioPrincipal.show();
+        
+        
+    }
+    
+}
